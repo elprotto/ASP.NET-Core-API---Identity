b2afddd8ca142a9a5987cd66bf564fa262c9351d
Alexander Protto <aprotto@hipercept.com>
2020-06-30 08:59:10 -0500

    Added Project structure

---
+++ ExampleProject/ExampleProject/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExampleProject.Models
+{
+    public class User: IdentityUser
+    {
+        public int CedulaId { get; set; }
+        public string MyCustomProperty { get; set; }
+        public virtual ICollection<IdentityRole> UserRoles { get; set; }
+    }
+}
